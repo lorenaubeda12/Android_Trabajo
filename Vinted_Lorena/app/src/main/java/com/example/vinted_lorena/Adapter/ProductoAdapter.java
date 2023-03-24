625ab71378eb9b54145a4de642ed6cc695539afa
Lore12Ubeda <lorenaubeda12@gmail.com>
2023-03-24 18:24:21 +0100

    Revert "continuar"
    This reverts commit 0ef77e850c53dc172642e9d5ad78cb532a07f5ce.

--- Vinted_Lorena/app/src/main/java/com/example/vinted_lorena/Adapter/ProductoAdapter.java
+++ Vinted_Lorena/app/src/main/java/com/example/vinted_lorena/Adapter/ProductoAdapter.java
@@ -139,6 +139,7 @@ public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.ViewHo
                     x.putExtra("usuario", g.toJson(u));
 
                     communication.showDetails(i);
+                    communication.showDetails(x);
                 }
             });
 
